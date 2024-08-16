b5481fd11073a2bf00b22160c67ea4febc5c23d2
Fabio-Canul <fabiocanul20@gmail.com>
2024-08-15 23:09:59 -0600

    Primera Versión

---
+++ script.js
@@ -0,0 +1,63 @@
+const textArea = document.querySelector(".text-area");
+const mensaje = document.querySelector(".mensaje");
+
+// La letra "e" es convertida para "enter"
+// La letra "i" es convertida para "imes"
+// La letra "a" es convertida para "ai"
+// La letra "o" es convertida para "ober"
+// La letra "u" es convertida para "ufat"
+
+function btnEncriptar(){
+    const textoEncriptado = encriptar(textArea.value)
+    mensaje.value = textoEncriptado
+    textArea.value = "";
+    mensaje.style.backgroundImage = "none"
+}
+
+function encriptar (stringEncriptada){
+    let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
+    stringEncriptada = stringEncriptada.toLowerCase()
+
+    for (let i = 0; i < matrizCodigo.length; i++){
+        if (stringEncriptada.includes(matrizCodigo[i][0])){
+            stringEncriptada = stringEncriptada.replaceAll(matrizCodigo[i][0],matrizCodigo[i][1])
+
+        }
+    }
+    return stringEncriptada
+}
+
+function btnDesencriptar(){
+    const textoEncriptado = desencriptar(textArea.value)
+    mensaje.value = textoEncriptado
+    textArea.value = "";
+}
+
+function desencriptar (stringDesencriptada){
+    let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
+    stringDesencriptada = stringDesencriptada.toLowerCase()
+
+    for (let i = 0; i < matrizCodigo.length; i++){
+        if (stringDesencriptada.includes(matrizCodigo[i][1])){
+            stringDesencriptada = stringDesencriptada.replaceAll(matrizCodigo[i][1],matrizCodigo[i][0])
+
+        }
+    }
+    return stringDesencriptada
+}
+
+function copiarMensaje(){
+    const copyText = copiado(mensaje.value);
+    textArea.value = copyText
+    mensaje.value = "";
+}
+
+function copiado (stringcopiar){
+    stringcopiar = stringcopiar.toLowerCase()
+    navigator.clipboard.writeText(stringcopiar).then(function(){
+        alert("¡Texto copiado en: ingrese el texto aqui!");
+    }).catch(function(error){
+        console.error("Error al copiar:", error);
+    });
+    return stringcopiar
+}
